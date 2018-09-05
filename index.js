be271d0af4e8606859cf5b6f89f209e1d43f7af7
raymondkenney@gmail.com <raymondkenney@gmail.com>
2018-09-05 11:40:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,43 +1,32 @@
 var animal = 'dog'
-
+ 
 function myAnimal() {
   return animal
 }
-
+ 
 function yourAnimal() {
-  var animal = 'cat';
+  var animal = 'cat'
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
-
+ 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return parseInt(n, 10) + two
+ 
+  // Feel free to move things around!
+ 
 }
-
+ 
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
 }
-
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
-
-
-var animal = 'dog';
  
-function makeZoo() {
-  var animal = 'cat';
- 
-  console.log(`I think I'll put this ${animal} in the zoo.`);
-}
- 
-makeZoo(); // "I think I'll put this cat in the zoo."
- 
-animal // "dog"
+// We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
+// NOTE: To pass this final test, you only need to modify the code below this line.
+var theFunk = funkyFunction()
+theFunk = theFunk()
